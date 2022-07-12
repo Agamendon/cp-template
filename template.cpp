52ccdce8493804f457bc3f4e8ecefe7bb6219267
Agamendon <gosha20052811@gmail.com>
2022-07-12 14:46:28 +0300

    Added Fenwick, SegTree (without lazy)

--- template.cpp
+++ template.cpp
@@ -86,4 +86,113 @@ ll Phi(ll k)
     return res;
 }
 
-// GRAPH ALGORITHMS
+
+// DATA STRUCTURES
+
+
+
+/**
+ * @brief 0-indexed Fenwick tree
+**/
+template <typename T>
+class FenwickTree {
+    private:
+        vector<T> __tree;
+        T (&__add_func)(T t1, T t2);
+        T __zerovalue;
+
+    public:
+        FenwickTree(int size, T zerovalue, T (&add_func)(T t1, T t2)) : __add_func(add_func)
+        {
+            __tree.resize(size);
+            fill(__tree.begin(), __tree.end(), zerovalue);
+            __zerovalue = zerovalue;
+        }
+
+        void update(int pos, T val)
+        {
+            int n = __tree.size();
+            pos++;
+            for(; pos <= n; pos += (pos & -pos))
+                __tree[pos - 1] = __add_func(__tree[pos - 1], val);
+            return;
+        }
+
+        T get(int pos)
+        {
+            T ans = __zerovalue;
+            pos++;
+            for(; pos > 0; pos -= (pos & -pos))
+                ans = __add_func(ans, __tree[pos - 1]);
+            return ans;
+        }
+};
+
+
+/**
+ * @brief 0-indexed segment tree
+**/
+template<typename T>
+class SegmentTree {
+    private:
+        int __size;
+        vector<T> __tree;
+        T (&__merge_function)(T t1, T t2);
+        T __zerovalue;
+
+        void __update(int l, int r, int t, int pos, T val)
+        {
+            if (l == r) {
+                __tree[t] = val;
+                return;
+            }
+            int lt = t * 2 + 1;
+            int rt = t * 2 + 2;
+            int m = (l + r) / 2;
+            if (pos <= m)
+                __update(l, m, lt, pos, val);
+            else
+                __update(m + 1, r, rt, pos, val);
+            __tree[t] = __merge_function(__tree[lt], __tree[rt]);
+            return;
+        }
+
+        T __get(int l, int r, int sl, int sr, int t)
+        {
+            if (l == sl && r == sr)
+                return tree[t];
+            int lt = t * 2 + 1;
+            int rt = t * 2 + 2;
+            int m = (l + r) / 2;
+            if (sr <= m)
+                return __get(l, m, sl, sr, lt);
+            else if (sl > m)
+                return __get(m + 1, r, sl, sr, rt);
+            else {
+                T lres = __get(l, m, sl, m, lt);
+                T rres = __get(m + 1, r, m + 1, sr, rt);
+                return __merge_function(lres, rres);
+            }
+        }
+
+    public:
+        SegmentTree(int size, T zerovalue, T (&merge_function)(T t1, T t2)) : __merge_function(merge_function)
+        {
+            __size = size;
+            __tree.resize(4 * size);
+            fill(__tree.begin(), __tree.end(), zerovalue);
+            __zerovalue = zerovalue;
+        }
+
+        void update(int pos, T val)
+        {
+            __update(0, __size - 1, 0, pos, val);
+            return;
+        }
+        
+        T get(int l, int r)
+        {
+            return __get(0, __size - 1, l, r, 0);
+        }
+};
+
